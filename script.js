35ac721e74211594a6c8719bc59d76fe7592ca78
dorsariahi <dorsariahiasl@gmail.com>
2022-10-03 19:35:05 -0500

    changed some typo

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   var passwordLenght = prompt("what is the lenght of the password?");
   if (passwordLenght >= 8 && passwordLenght <= 128) {
-    var hasLowerCase = confirm("do you want to include lowercase charcter?");
-    var hasUpperCase = confirm("do you want to include uppercase charcter?");
-    var hasSpecialCharcter = confirm("do you want to include special charcters?");
-    var hasNumeric = confirm("do you want to include numeric characters?");
+    var hasLowerCase = confirm("Do you want to include any lowercase charcters?");
+    var hasUpperCase = confirm("Do you want to include any uppercase charcters?");
+    var hasSpecialCharcter = confirm("Do you want to include any special charcters?");
+    var hasNumeric = confirm("Do you want to include any numeric characters?");
     if (hasLowerCase === true || hasUpperCase === true || hasSpecialCharcter === true || hasNumeric === true) {
       var lowercase = "abcdefghijklmnopqrstuvwxyz"
       var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
